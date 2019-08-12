cb620a5f36bd3d0d3cf7c93a5cae96246b76f606
Dinesh1121 <53996436+Dinesh1121@users.noreply.github.com>
2019-08-12 11:14:28 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the value of n: "))
+sum = 0
+if num <= 0: 
+   print("Enter a whole positive number!") 
+else: 
+   while num > 0:
+        sum = sum + num
+        num = num - 1;
+   print("Sum is: ", sum)
